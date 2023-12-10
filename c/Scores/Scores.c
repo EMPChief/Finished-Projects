1424ca2032c7c4c1fe6f65a782db0dee9d782d61
EMP <70442425+EMPChief@users.noreply.github.com>
2023-12-10 20:06:37 +0100

    Create Scores.c
    Created so can avarage school grades

---
+++ c/Scores/Scores.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void)
+{
+    // Declare a variable to store the number of scores
+    int number;
+
+    // Use a do-while loop to ensure a valid number of scores is entered
+    do
+    {
+        number = get_int("Type how many scores you want to use: ");
+    } while (number <= 0);
+
+    // Declare an array to store the scores based on the user-specified number
+    int scores[number];
+
+    // Use a for loop to input the scores based on the specified number
+    for (int i = 0; i < number; ++i)
+    {
+        scores[i] = get_int("Score number: ");
+    }
+
+    // Initialize a variable to store the sum of the scores
+    float sum = 0;
+
+    // Use a for loop to calculate the sum of the scores
+    for (int i = 0; i < number; ++i)
+    {
+        sum += scores[i];
+    }
+
+    // Calculate and print the average school grade
+    printf("Average school grade is: %f\n", sum / (float)number);
+
+    // Indicate successful completion of the program
+    return 0;
+}
