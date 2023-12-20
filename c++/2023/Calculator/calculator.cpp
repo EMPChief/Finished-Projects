87d33964994d5f6dd7186917d3e5abba3d09417a
empchief <bjornmagnekristensen1999@gmail.com>
2023-12-20 17:52:57 +0100

    changed order off operator so ask what want to use first

--- c++/2023/Calculator/calculator.cpp
+++ c++/2023/Calculator/calculator.cpp
@@ -5,11 +5,11 @@ int main()
     double num1, num2;
     char op;
 
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
-
     std::cout << "Enter operator (+, -, *, /): ";
     std::cin >> op;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
 
     std::cout << "Enter second number: ";
     std::cin >> num2;
